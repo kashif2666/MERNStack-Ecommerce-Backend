2c9311c8d366a7a7f4cb6678c6f745fb25568a19
kashif2666 <kashifjsr7@gmail.com>
2024-05-09 16:43:47 +0530

    Backend Part 8: Stripe integration

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 
 const { createProduct } = require("./controller/Product");
-const productsRouters = require("./routes/Products");
-const categoriesRouters = require("./routes/Categories");
-const brandsRouters = require("./routes/Brands");
-const usersRouters = require("./routes/Users");
-const authRouters = require("./routes/Auth");
-const cartRouters = require("./routes/Cart");
-const ordersRouters = require("./routes/Order");
+const productsRouter = require("./routes/Products");
+const categoriesRouter = require("./routes/Categories");
+const brandsRouter = require("./routes/Brands");
+const usersRouter = require("./routes/Users");
+const authRouter = require("./routes/Auth");
+const cartRouter = require("./routes/Cart");
+const ordersRouter = require("./routes/Order");
 const { User } = require("./model/User");
 const { isAuth, sanitizeUser, cookieExtractor } = require("./services/common");
 
@@ -49,14 +49,15 @@ server.use(
     exposedHeaders: ["X-Total-Count"],
   })
 );
+server.use(express.raw({ type: "application/json" }));
 server.use(express.json()); // to parse req.body
-server.use("/products", isAuth(), productsRouters.router); // we can also use JWT token for client-only auth
-server.use("/categories", isAuth(), categoriesRouters.router);
-server.use("/brands", isAuth(), brandsRouters.router);
-server.use("/users", isAuth(), usersRouters.router);
-server.use("/auth", authRouters.router);
-server.use("/cart", isAuth(), cartRouters.router);
-server.use("/orders", isAuth(), ordersRouters.router);
+server.use("/products", isAuth(), productsRouter.router); // we can also use JWT token for client-only auth
+server.use("/categories", isAuth(), categoriesRouter.router);
+server.use("/brands", isAuth(), brandsRouter.router);
+server.use("/users", isAuth(), usersRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart", isAuth(), cartRouter.router);
+server.use("/orders", isAuth(), ordersRouter.router);
 
 //passport strategies
 passport.use(
@@ -130,6 +131,67 @@ passport.deserializeUser(function (user, cb) {
     return cb(null, user);
   });
 });
+
+// Payments
+// This is your test secret API key.
+const stripe = require("stripe")(
+  "sk_test_51PEBdESJGas4etDRvNUxFR3Z5GYVghRe3Ijsjbl47on9NpHy3nN3Nu7z1i3qe6xk4QgAGUgV5fHT7N4PxtTsT7CK00KZf6EFNv"
+);
+
+server.post("/create-payment-intent", async (req, res) => {
+  const { totalAmount } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: totalAmount * 100,
+    currency: "inr",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// webhook
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret =
+  "whsec_ec28859f2c6dd73f6d250ba6e553cb75e7773db2b29dcd3c0da7750c44c7402f";
+
+server.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntentSucceeded = event.data.object;
+        console.log("this is success", { paymentIntentSucceeded });
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
 
 main().catch((err) => console.log(err));
 
